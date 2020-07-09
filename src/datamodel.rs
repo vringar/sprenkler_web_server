ec44283efb71b91a13a0ff8494d9eea8d449d4a1
vringar <h_dieter@mail.ru>
2020-07-09 19:43:03 +0200

    moved a bunch of stuff

--- src/datamodel.rs
+++ src/datamodel.rs
@@ -1,19 +1,33 @@
-use serde::{Serialize, Deserialize};
+use serde::{Serialize, Serializer, Deserialize};
+use reqwest::Url;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Schedule {
     status: bool
 }
-
+#[derive(Serialize, Deserialize,Debug)]
+pub enum Status {
+    ScheduledOpen,
+    ScheduledClosed,
+    OverrideOn,
+    OverrideOff
+}
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Valve {
     pub name: String,
     pub index: i8,
+    pub status: Status,
     schedule: Schedule
 }
 
 impl Valve {
     pub fn new(name: &str, index: i8) -> Self {
-        Valve{name: name.to_owned(), index: index, schedule: Schedule{status: false}}
+        Valve{name: name.to_owned(), index: index, status: Status::OverrideOff, schedule: Schedule{status: false}}
     }
+}
+
+#[derive(Debug)]
+pub struct Config {
+    pub valves: Vec<Valve>,
+    pub adress: Url
 }
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@ use serde::Serialize;
 use serde_json::json;
 use warp::Filter;
 
+use reqwest::Url;
+
 mod datamodel;
+use datamodel::Config;
 use datamodel::Valve;
-static INDEX_HBS: &'static str = include_str!("templates/index.hbs");
 
 struct WithTemplate<T: Serialize> {
     name: &'static str,
@@ -24,14 +26,11 @@ where
     warp::reply::html(render)
 }
 
-
 #[tokio::main]
 async fn main() {
-    let template = INDEX_HBS;
-
     let mut hb = Handlebars::new();
     // register the template
-    hb.register_template_string("index.html", template)
+    hb.register_templates_directory(".hbs", "./static/templates")
         .unwrap();
 
     // Turn Handlebars instance into a Filter so we can combine it
@@ -39,25 +38,37 @@ async fn main() {
     let hb = Arc::new(hb);
 
     // Create a reusable closure to render template
-    let handlebars = move |with_template| render(with_template, hb.clone());
+    let handlebars = {
+        let hb = hb.clone();
+        move |with_template| render(with_template, hb.clone())
+    };
 
+
+    let url = Url::parse("https://localhost:4040").unwrap();
+    let config: Config = Config {
+        adress: url,
+        valves: vec![Valve::new("blub", 0)],
+    };
+    let config = Arc::new(config);
     //GET /
     let root = warp::path::end()
-        .map(|| {
-            let test = vec!{Valve::new("blub", 0)};
-            let test = json!({"Valves":test});
-            println!("{}",test);
-            WithTemplate {
-                name: "index.html",
-                value: test,
-            }
-        }
-           )
-        .map(handlebars);
+        .map(|| WithTemplate {
+            name: "index",
+            value: json!({ "Valves": vec![Valve::new("blub", 0)] }),
+        })
+        .map(handlebars.clone());
 
     let static_content = warp::path("static").and(warp::fs::dir("./static/"));
 
+    let valve = warp::path("valves")
+        .and(warp::path::param())
+        .and(warp::path::end())
+        .map(|_: i16| WithTemplate {
+            name: "details",
+            value: json!(Valve::new("blub", 0)),
+        })
+        .map(handlebars.clone());
 
-    let route = warp::get().and(root.or(static_content));
+    let route = warp::get().and(root.or(static_content).or(valve));
     warp::serve(route).run(([127, 0, 0, 1], 3030)).await;
 }
