837731c7132ac47101be244843ca47840f1ff79c
vringar <h_dieter@mail.ru>
2020-07-10 16:02:33 +0200

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -70,7 +70,7 @@ async fn main() {
 fn get_valve_paths(hb: Arc<Handlebars> , config: Arc<Config> ) -> impl Filter<Extract = (impl warp::Reply,), Error = Rejection> + Clone + '_{
         let hb = hb.clone();
         let handlebars = move |with_template| render(with_template, hb.clone());
-        let a = warp::path("valves")
+        warp::path("valves")
         .and(warp::path::param())
         .and(warp::path::end())
         .map(move |i: usize| WithTemplate {
@@ -79,6 +79,5 @@ fn get_valve_paths(hb: Arc<Handlebars> , config: Arc<Config> ) -> impl Filter<Ex
                 (*config).valves[i]
             ),
         })
-        .map(handlebars.clone());
-        a
+        .map(handlebars.clone())
 }
