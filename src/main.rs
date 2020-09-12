70453b857685aff71fab8659aded475af2d53e0b
vringar <h_dieter@mail.ru>
2020-09-12 18:41:06 +0200

    Added tracing

--- src/main.rs
+++ src/main.rs
@@ -21,8 +21,26 @@ use hb::WithTemplate;
 mod datamodel;
 use datamodel::{ControllerConfig, ServerConfig, Valve};
 
+use tracing_subscriber::fmt::format::FmtSpan;
+
 #[tokio::main]
 async fn main() {
+    // Filter traces based on the RUST_LOG env var, or, if it's not set,
+    // default to show the output of the example.
+    let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "tracing=info,warp=debug".to_owned());
+
+    // Configure the default `tracing` subscriber.
+    // The `fmt` subscriber from the `tracing-subscriber` crate logs `tracing`
+    // events to stdout. Other subscribers are available for integrating with
+    // distributed tracing systems such as OpenTelemetry.
+    tracing_subscriber::fmt()
+        // Use the filter we built above to determine which traces to record.
+        .with_env_filter(filter)
+        // Record an event when each span closes. This can be used to time our
+        // routes' durations!
+        .with_span_events(FmtSpan::CLOSE)
+        .init();
+
     let mut hb = Handlebars::new();
     // register the template
     hb.register_templates_directory(".hbs", "./static/templates")
@@ -59,7 +77,10 @@ async fn main() {
         .and(warp::path("static"))
         .and(warp::fs::dir("./static/"));
 
-    let routes = root.or(static_content).or(valve_paths);
+    let routes = root
+        .or(static_content)
+        .or(valve_paths)
+        .with(warp::trace::request());
     // hyper let's us build a server from a TcpListener (which will be
     // useful shortly). Thus, we'll need to convert our `warp::Filter` into
     // a `hyper::service::MakeService` for use with a `hyper::server::Server`.
