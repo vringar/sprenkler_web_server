2164b709dca1d4d03128d9efe40e172f67076508
vringar <h_dieter@mail.ru>
2021-02-07 15:58:19 +0100

    Starting work on this again

--- src/hb.rs
+++ src/hb.rs
@@ -1,7 +1,8 @@
 use serde::Serialize;
 use std::sync::Arc;
 
-use handlebars::Handlebars;
+use handlebars::{Handlebars, handlebars_helper};
+
 pub struct WithTemplate<T: Serialize> {
     pub name: &'static str,
     pub value: T,
@@ -15,4 +16,26 @@ where
         .render(template.name, &template.value)
         .unwrap_or_else(|err| err.to_string());
     warp::reply::html(render)
+}
+
+pub fn init() -> Handlebars<'static> {
+    let mut hb = Handlebars::new();
+    handlebars_helper!(ifeq: |this:str, other:str|  this.eq(other));
+    hb.register_helper("ifeq", Box::new(ifeq));
+    // register the template
+    hb.register_templates_directory(".hbs", "./static/templates")
+        .unwrap();
+    hb
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::init;
+    #[test]
+    fn test_helper() {
+        let hb = init();
+        assert_eq!(2 + 2, 4);
+    }
 }
